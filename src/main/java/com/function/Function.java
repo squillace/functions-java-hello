564586fcb33802681c160f2c6383871852a02a1f
Jeff Hollan <jeffhollan@outlook.com>
2020-03-08 14:06:55 -0700

    modified to get weather

--- src/main/java/com/function/Function.java
+++ src/main/java/com/function/Function.java
@@ -1,34 +1,36 @@
 package com.function;
 
+import java.io.IOException;
 import java.util.*;
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHeaders;
+import org.apache.http.ParseException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
 /**
- * Azure Functions with HTTP Trigger.
+ * Azure Functions with HTTP Trigger to get the weather.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpTrigger-Java". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTrigger-Java&code={your function key}
-     * 2. curl "{your host}/api/HttpTrigger-Java?name=HTTP%20Query&code={your function key}"
-     * Function Key is not needed when running locally, it is used to invoke function deployed to Azure.
-     * More details: https://aka.ms/functions_authorization_keys
-     */
+
+    private static CloseableHttpClient httpClient = HttpClients.createDefault();
+
     @FunctionName("HttpTrigger")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
+    public HttpResponseMessage run(@HttpTrigger(name = "req", methods = { HttpMethod.GET,
+            HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) throws ParseException, IOException {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
+        // Get the current weather at Microsoft
+        HttpGet weatherRequest = new HttpGet("https://api.darksky.net/forecast/3ac036c93fab776bb5ae3f1451070a1a/47.639880,-122.124380");
+        CloseableHttpResponse weatherResponse = httpClient.execute(weatherRequest);
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Hello!\n You can pass in a name as a query parameter (name) or in the request body for a personalized greeting").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
-        }
+        return request.createResponseBuilder(HttpStatus.OK).body(EntityUtils.toString(weatherResponse.getEntity())).build();
     }
 }
